56e615b1f9c1d6afb9fc2ff5d95c8d2b928c2776
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-07-21 08:35:45 +1000

    Start of a UQ Theme (Material colors)

--- src/index.js
+++ src/index.js
@@ -26,6 +26,24 @@ var mock = mock || false;
 
   /** @ngInject */
   function appConfig($mdThemingProvider) {
-    $mdThemingProvider.theme('default');
+    var uqPurple = $mdThemingProvider.extendPalette('deep-purple', {
+      '300': '#d2c1d7',
+      '400': '#6B0C8A',
+      '500': '#49075E',
+      '600': '#370546'
+    });
+    $mdThemingProvider.definePalette('uqPurple', uqPurple);
+
+    var uqBlue = $mdThemingProvider.extendPalette('blue', {
+      '300': '#D9EEFF',
+      '400': '#71A3F7',
+      '500': '#4285f4',
+      '600': '#005EA5'
+    });
+    $mdThemingProvider.definePalette('uqBlue', uqBlue);
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('uqPurple')
+      .accentPalette('uqBlue');
   }
 })();
